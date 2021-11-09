958545ea1899b77b6790e27c894b7517e5962e91
vautrin33u <vautrin33u@etu.univ-lorraine.fr>
2021-11-09 19:37:12 +0100

    création des tests

--- Model.cpp
+++ Model.cpp
@@ -4,7 +4,6 @@
 
 #include "Model.hpp"
 #include <cctype>
-#include <QString>
 
 Model::Model(){}
 
---
+++ v1/tests/tests.hpp
@@ -0,0 +1,14 @@
+//
+// Created by guy on 09/11/2021.
+//
+
+#ifndef SHARECOUNT_TESTS_HPP
+#define SHARECOUNT_TESTS_HPP
+
+
+class tests {
+
+};
+
+
+#endif //SHARECOUNT_TESTS_HPP
